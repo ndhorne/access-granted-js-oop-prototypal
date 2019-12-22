68a34b358062d1dac87e34be73caa7ac69f5d959
Nicholas D. Horne <55657484+ndhorne@users.noreply.github.com>
2019-12-22 03:36:06 -0800

    Updated about dialog

--- accessgranted.js
+++ accessgranted.js
@@ -130,7 +130,9 @@ Game.prototype.about = function about(event) {
     "resulting in PINs with less than four keys being\n" +
     "highlighted. PINs may begin with zero. Input is\n" +
     "accepted by way of both mouse primary button\n" +
-    "and keyboard number keys.");
+    "and keyboard number keys.\n" +
+    "\n" +
+    "Source available at https://github.com/ndhorne");
   event.preventDefault();
 };
 
